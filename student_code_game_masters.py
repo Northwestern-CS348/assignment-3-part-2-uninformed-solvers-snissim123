c81de26727e67b0e6b9eae9256e5caaa40453006
snissim123 <sandra.nissim@gmail.com>
2019-02-21 17:44:32 -0600

    hanoi working

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -10,7 +10,6 @@ class TowerOfHanoiGame(GameMaster):
     def produceMovableQuery(self):
         """
         See overridden parent class method for more information.
-
         Returns:
              A Fact object that could be used to query the currently available moves
         """
@@ -23,45 +22,68 @@ class TowerOfHanoiGame(GameMaster):
         represent a peg, and its content the disks on the peg. Disks
         should be represented by integers, with the smallest disk
         represented by 1, and the second smallest 2, etc.
-
         Within each inner Tuple, the integers should be sorted in ascending order,
         indicating the smallest disk stacked on top of the larger ones.
-
         For example, the output should adopt the following format:
         ((1,2,5),(),(3, 4))
-
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### student code goes here
-        pass
+        allDisks = [parse_input("fact: (on ?x peg1)"), parse_input("fact: (on ?x peg2)"), parse_input("fact: (on ?x peg3)")]
+
+        allBindings = [self.kb.kb_ask(i) for i in allDisks]
+        numList = []
+
+        for i in allBindings:
+            if i:
+                miniList = []
+                for j in i:
+                    
+                    for k in j.bindings:
+                        miniList.append(int(str(k)[-1]))
+                        miniList.sort()
+                numList.append(tuple(miniList))
+            else:
+                numList.append(tuple())
+
+        numList = tuple(numList)
+        return numList
 
     def makeMove(self, movable_statement):
         """
         Takes a MOVABLE statement and makes the corresponding move. This will
         result in a change of the game state, and therefore requires updating
         the KB in the Game Master.
-
         The statement should come directly from the result of the MOVABLE query
         issued to the KB, in the following format:
         (movable disk1 peg1 peg3)
-
         Args:
             movable_statement: A Statement object that contains one of the currently viable moves
-
         Returns:
             None
         """
         ### Student code goes here
-        pass
+        stateTerms = movable_statement.terms
+        dstPeg = stateTerms[2]
+        srcPeg = stateTerms[1]
+        disk = stateTerms[0]
+
+        self.kb.kb_retract(Fact(["top", disk, srcPeg]))
+        self.kb.kb_retract(Fact(["on", disk, srcPeg]))
+        newTop = str(self.kb.kb_ask(Fact(["topOf", disk, "?disk"]))[0].bindings[0].constant)
+        self.kb.kb_assert(Fact(["top", newTop, srcPeg]))
+        self.kb.kb_assert(Fact(["on", disk, dstPeg]))
+        self.kb.kb_retract(Fact(["topOf", disk, newTop]))
+        oldTop = str(self.kb.kb_ask(Fact(["top", "?disk", dstPeg]))[0].bindings[0].constant)
+        self.kb.kb_assert(Fact(["topOf", disk, oldTop]))
+        self.kb.kb_retract(Fact(["top", oldTop, dstPeg]))
+        self.kb.kb_assert(Fact(["top", disk, dstPeg]))
 
     def reverseMove(self, movable_statement):
         """
         See overridden parent class method for more information.
-
         Args:
             movable_statement: A Statement object that contains one of the previously viable moves
-
         Returns:
             None
         """
@@ -69,6 +91,7 @@ class TowerOfHanoiGame(GameMaster):
         sl = movable_statement.terms
         newList = [pred, sl[0], sl[2], sl[1]]
         self.makeMove(Statement(newList))
+
 
 class Puzzle8Game(GameMaster):
 
@@ -100,7 +123,29 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
-        pass
+
+        myList = [[0, 0, 0], [0, 0,0], [0, 0,0]]
+        allTiles = self.kb.kb_ask(parse_input("fact: (pos ?tile ?posx ?posy)"))
+        
+        for t in allTiles:
+            myTile = str(t.bindings_dict["?tile"][4])
+            
+            if myTile == "y":
+                myTile = -1
+            else:
+                myTile = int(myTile)
+
+            row = int(t.bindings_dict["?posx"][-1]) - 1
+            col = int(t.bindings_dict["?posy"][-1]) - 1
+
+            myList[col][row] = myTile
+
+        for i in range(len(myList)):
+            myList[i] = tuple(myList[i])
+
+        return tuple(myList)
+        
+
 
     def makeMove(self, movable_statement):
         """
@@ -119,7 +164,21 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+
+        stateTerms = movable_statement.terms
+        dstY = stateTerms[4]
+        dstX = stateTerms[3]
+        srcY = stateTerms[2]
+        srcX = stateTerms[1]
+        tile = stateTerms[0]
+        if self.kb.kb_ask(Fact(["pos", "?empty", dstX, dstY])):
+            self.kb.kb_retract(Fact(["pos", tile, srcX, srcY]))
+            # move empty block to previous
+            self.kb.kb_retract(Fact(["pos", "?empty", dstX, dstY]))
+            self.kb.kb_assert(Fact(["pos", tile, dstX, dstY]))
+            self.kb.kb_assert(Fact(["pos", "empty", srcX, srcY]))
+            
+            
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,5 +1,5 @@
-
 from solver import *
+from collections import deque
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
@@ -14,17 +14,66 @@ class SolverDFS(UninformedSolver):
         If all game states reachable from a parent state has been explored,
         the next explored state should conform to the specifications of
         the Depth-First Search algorithm.
-
         Returns:
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
 
+        # Check to see if condition is met
+        if self.gm.isWon():
+            return True
+
+
+        # mark as visited
+        self.visited[self.currentState] = True
+        # generate available moves
+        possMoves = self.gm.getMovables()
+
+        # go through each possible move
+        for move in possMoves:
+            self.gm.makeMove(move)
+
+            gameState = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+            gameState.parent = self.currentState
+
+
+            if gameState not in self.visited:
+                self.currentState.children.append(gameState)
+                self.visited[gameState] = False
+
+            elif self.visited[gameState] == False:
+                self.currentState.children.append(gameState)
+
+            self.gm.reverseMove(move)
+
+        # Which child we should be looking at 
+        childNum = self.currentState.nextChildToVisit
+
+        while self.currentState.depth != 0 and childNum == len(self.currentState.children):
+            childNum = self.currentState.nextChildToVisit
+
+            self.currentState = self.currentState.parent
+
+            self.gm.reverseMove(self.currentState.requiredMovable)
+
+
+        childNum = self.currentState.nextChildToVisit
+
+        if self.visited[self.currentState.children[childNum]] == False:
+            self.currentState.nextChildToVisit += 1
+
+            self.gm.makeMove(self.currentState.children[childNum].requiredMovable)
+
+            self.currentState = self.currentState.children[childNum]
+
+        return False
+   
 
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.myList = dict()
+        self.myQueue = deque()
 
     def solveOneStep(self):
         """
@@ -35,9 +84,51 @@ class SolverBFS(UninformedSolver):
         If all game states reachable from a parent state has been explored,
         the next explored state should conform to the specifications of
         the Breadth-First Search algorithm.
-
         Returns:
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        return True
+
+        # Check to see if condition is met
+        if self.gm.isWon():
+            return True
+
+        # mark as visited
+        self.visited[self.currentState] = True
+        # generate available moves
+        possMoves = self.gm.getMovables()
+
+        if not self.currentState.depth:
+            self.myList[self.currentState] = []
+
+        # go through each possible move
+        for move in possMoves:
+            self.gm.makeMove(move)
+            gameState = GameState(self.gm.getGameState(), self.currentState.depth + 1, move)
+            gameState.parent = self.currentState
+
+            if gameState not in self.visited:
+                self.currentState.children.append(gameState)
+                self.visited[gameState] = False
+                self.myQueue.append(gameState)
+                
+                self.myList[gameState] = []
+                for elem in self.myList[self.currentState]:
+                    self.myList[gameState].append(elem)
+                self.myList[gameState].append(gameState)
+                
+            self.gm.reverseMove(move)
+
+        # Find list of moves before
+        before = self.myList[self.currentState]
+        before.reverse()
+        self.currentState = self.myQueue.popleft()
+        # undo those moves
+        for move in before: 
+            self.gm.reverseMove(move.requiredMovable)
+        after = self.myList[self.currentState]
+        for move in after: 
+            self.gm.makeMove(move.requiredMovable)
+
+        return False
+        
